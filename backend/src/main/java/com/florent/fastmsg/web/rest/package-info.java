e2ef7bb02d473247536a3ccc1c2a8867b3f79c1f
florentL <florent.le.boulch@gmail.com>
2018-10-27 11:58:39 +0200

    Generated with JHipster 5.3.4

---
+++ backend/src/main/java/com/florent/fastmsg/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.florent.fastmsg.web.rest;
---
+++ backend/src/main/java/com/florent/fastmsg/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package com.florent.fastmsg.web.websocket.dto;
