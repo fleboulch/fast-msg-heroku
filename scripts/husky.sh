d04d07624a339f1b89dfc869b190f7ec5e768456
florentL <florent.le.boulch@gmail.com>
2018-12-16 14:30:42 +0100

    Prepare jhi upgrade

--- scripts/husky.sh
+++ scripts/husky.sh
@@ -4,14 +4,15 @@ baseUrl='https://api.github.com/repos/fleboulch/fast-msg-heroku'
 
 # env -i is a workaround (see: https://github.com/typicode/husky/issues/364)
 currentBranchName=$(env -i git rev-parse --abbrev-ref HEAD)
+echo $currentBranchName
 prUrl="$baseUrl/pulls?state=opened&head=fleboulch:$currentBranchName"
-prExisting=$(curl -s $prUrl | jq length)
+prExisting=$(curl -u "fleboulch" -s $prUrl | jq length)
 
 if [[ $prExisting -eq 0 ]]; then
 
     file='../app.json'
     issuesUrl="$baseUrl/issues?state=all"
-    nbIssues=$(curl -s $issuesUrl | jq length)
+    nbIssues=$(curl -u "fleboulch" -s $issuesUrl | jq length)
     newPrNb=$(($nbIssues+1))
     pattern="s/pr-\([0-9]*\)/pr-$newPrNb/"
 
@@ -20,8 +21,8 @@ if [[ $prExisting -eq 0 ]]; then
     echo "New PR will be #$newPrNb"
 
     sed -i -e $pattern $file
-    git add $file
-    git commit --amend --no-edit
+    #env -i git add $file
+    #env -i git commit --amend --no-edit
 else
     echo "PR has already been created"
 fi
